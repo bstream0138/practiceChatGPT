4d0a68b8160647ec0341cd22014b76c9b8da2a31
bstream0138 <bstream0138@gmail.com>
2023-11-15 10:06:54 +0900

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <div className="bg-red-100 min-h-screen max-w-screen-md mx-auto flex">
+      <div className="bg-black w-52">Side </div>
+      <div className="bg-green-100 grow flex flex-col">
+        <div className="bg-gray-100 grow">Main </div>
+        <div className="bg-gray-300 h-24">Chat Bar</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
